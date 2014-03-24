b10351aaf247bdb32443160d1b4c5f20cfac1b4f
facu.tk <facu.tk@gmail.com>
2014-03-24 00:49:02 -0300

    jQuery color REST

--- fsk-server/server.py
+++ fsk-server/server.py
@@ -1,25 +1,23 @@
 from flask import Flask
 from werkzeug.contrib.cache import SimpleCache
-from flask import send_file, jsonify
+from flask import jsonify, request, send_file
 cache = SimpleCache()
 app = Flask(__name__)
 
-@app.route('/color/<param>')
-def color(param):
-    cache.set('color', param)
-    return 'ok'
+@app.route('/color', methods=['GET', 'POST'])
+def color():
+    if request.method == 'POST':
+        color = request.form['color']
+        cache.set('color', color)
+        return jsonify( status = 'ok', color = color )
+    else:
+        color = cache.get('color')
+        return jsonify( status = 'ok', color = color )
 
 @app.route('/')
 def index():
-    #color = cache.get('color')
-    #return '<body bgcolor="%s">'%(color)
-    #return send_file('templates/index.html')
     return send_file('templates/index.html')
-
-@app.route('/greeting')
-def greeting():
-    return jsonify( id = 1, content = 'dance with dragons' )
 
 if __name__ == '__main__':
     cache.set('color', '000000')
-    app.run()
+    app.run(host='0.0.0.0')
